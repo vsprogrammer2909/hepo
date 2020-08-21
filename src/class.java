577f1bf34818cae71b813e7426303b0373146c20
vsprogrammer2909 <69761145+vsprogrammer2909@users.noreply.github.com>
2020-08-21 12:56:52 +0530

    new pattern 27

--- src/class.java
+++ src/class.java
@@ -1103,3 +1103,25 @@ class Pattern26
     }
 }
 
+class pattern27
+{
+    public static void main(String args[])
+    {
+        int sp=0,sy=9;
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=sp;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int k=1;k<=sy;k++)
+            {
+                System.out.print(k);
+            }
+            System.out.println();
+            sp++;
+            sy-=2;
+        }
+    }
+
+}
